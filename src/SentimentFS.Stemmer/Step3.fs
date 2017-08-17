1d7067124c90485dccbf7b27a0c138adeed32a3c
Dominik Kotecki <dominikus1910@gmail.com>
2017-08-17 19:57:21 +0200

    add STep3 file

---
+++ src/SentimentFS.Stemmer/Step3.fs
@@ -0,0 +1,8 @@
+namespace SentimentFS.Stemmer.Steps
+
+module Step3 =
+    open System.Text.RegularExpressions
+    open SentimentFS.TextUtilities.Text
+    open SentimentFS.TextUtilities.Regex
+    open SentimentFS.Stemmer.Rules
+    open SentimentFS.Stemmer
