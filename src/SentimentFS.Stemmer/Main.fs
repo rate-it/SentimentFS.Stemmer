647297b21b5d9b7311ea19574061a722fbf3027d
Dominik Kotecki <Dominik.Kotecki@rossmann.com.pl>
2017-08-18 10:46:05 +0200

    add STemmer tests

--- src/SentimentFS.Stemmer/Main.fs
+++ src/SentimentFS.Stemmer/Main.fs
@@ -1,7 +1,13 @@
 namespace SentimentFS.Stemmer
 
 module Stemmer =
+    open SentimentFS.TextUtilities.Text
 
     [<CompiledName("Stem")>]
-    let rec stem = 2
-
+    let rec stem =
+        let localstem = fun (word: string) ->
+            if word.Length <= 2 then
+                word
+            else
+                word |> toLower |> SpecialWord.apply |> PostSpecialWord.apply
+        localstem
---
+++ tests/SentimentFS.Stemmer.Tests/Stemmer.fs
@@ -0,0 +1,44 @@
+namespace SentimentFS.Stemmer.Tests.Steps
+
+module Stemmer =
+    open Expecto
+    open SentimentFS.Stemmer.Steps
+    open SentimentFS.Stemmer
+    open System.IO
+    let testsFromOfficialDiffs =
+        seq {
+            use diffs = System.Reflection.Assembly.GetEntryAssembly().GetManifestResourceStream("diffs.txt")
+            let reader = new StreamReader(diffs)
+            let mutable line = reader.ReadLine()
+            while not (isNull line) do
+                let split = line.Split([| ' ' |], System.StringSplitOptions.RemoveEmptyEntries)
+                let word = split.[0]
+                let expected = split.[1]
+
+                let test = testCase word <| fun _ ->
+                    let actual = Stemmer.stem(word)
+                    Expect.equal actual (expected) (sprintf "expected %s; got %s" expected actual)
+
+                yield test
+                line <- reader.ReadLine()
+        }
+    let officialTests = Seq.toList testsFromOfficialDiffs
+
+    [<Tests>]
+    let tests =
+        testList "stem" [
+            testCase "Hello" <| fun _ ->
+                let subject = Stemmer.stem("Hello")
+                Expect.equal subject ("hello") "should equal hello"
+            testCase "hElLo" <| fun _ ->
+                let subject = Stemmer.stem("hElLo")
+                Expect.equal subject ("hello") "should equal hello"
+            testCase "capabilities" <| fun _ ->
+                let subject = Stemmer.stem("capabilities")
+                Expect.equal subject ("capabl") "should equal capabl"
+            testCase "expressive" <| fun _ ->
+                let subject = Stemmer.stem("expressive")
+                Expect.equal subject ("express") "should equal express"
+
+            testList "official" officialTests
+        ]
